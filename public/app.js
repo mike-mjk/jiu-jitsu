c73660ba393e5259d9679784894a733d0dc03f6b
Michael Kostolnik <mikeavida@gmail.com>
2017-02-24 06:18:46 +0000

    connect to youtube api and add video object to MOCK VIDEOS

--- public/app.js
+++ public/app.js
@@ -2,24 +2,27 @@ var MOCK_VIDEOS = {
     videos: [
         {
             id: "UNr5uyJ5fKM",
-            vidTitle: "Daily BJJ: No Gi Mount Basics + Armbar",
+            title: "Daily BJJ: No Gi Mount Basics + Armbar",
             channelTitle: "Carnage BJJ",
             thumbnail: "https://i.ytimg.com/vi/UNr5uyJ5fKM/mqdefault.jpg",
-            category: "armbar"
+            description: "description text 1",
+            tags: ['tag1', 'tag2', 'tag3']
         },
         {
             id: "kkHzgDaSkHY",
-            vidTitle: "UnStoppable Arm Bar Setup From The Mount NO-GI Tutorial With Darren Goodall",
+            title: "UnStoppable Arm Bar Setup From The Mount NO-GI Tutorial With Darren Goodall",
             channelTitle: "VenomFIT Online Fitness Program",
             thumbnail: "https://i.ytimg.com/vi/kkHzgDaSkHY/mqdefault.jpg",
-            category: "armbar"
+            description: "description text 2",
+            tags: ['tag1', 'tag2', 'tag3']
         },
         {
             id: "61BH9zLZct0",
-            vidTitle: "Marcelo Garcia Standing Step Pass",
+            title: "Marcelo Garcia Standing Step Pass",
             channelTitle: "Abu Enes",
             thumbnail: "https://i.ytimg.com/vi/61BH9zLZct0/mqdefault.jpg",
-            category: "pass"
+            description: "description text 2",
+            tags: ['tag1', 'tag2', 'tag3']
         }
         ]
 };
@@ -34,19 +37,8 @@ function getCategoryVideos(callbackFn) {
     // we use a `setTimeout` to make this asynchronous
     // as it would be with a real AJAX call.
 	setTimeout(function(){ callbackFn(MOCK_VIDEOS)}, 1);
+	console.log('these are ' + JSON.stringify(MOCK_VIDEOS));
 }
-
-// this function stays the same when we connect
-// to real API later
-// The commented out version below contains code for appending to correct category and is also messy
-//function displayCategoryVideos(data) {
-//     for (index in data.videos) {
-//         var cat = data.videos[index].category;
-//         var correctList = $('#' + cat);
-// 	    correctList.append(
-//         '<li id="' + data.videos[index].id + '">' + data.videos[index].vidTitle + '<br />' + data.videos[index].channelTitle + '<br />' + '<a href="/' + data.videos[index].id +'"><img src=' + data.videos[index].thumbnail + '>' + '</a></li>');
-//     }
-// }
 
 // this function stays the same when we connect
 // to real API later (version without categories and hopefully not messy)
@@ -66,17 +58,52 @@ function getAndDisplayVideos() {
 	getCategoryVideos(displayCategoryVideos);
 }
 
+var baseURL = 'https://www.googleapis.com/youtube/v3/videos'
 
-// $('ul').on('click', 'li', function(){
-//     var id = $(this).attr('id');
-//     $('#ytplayer').attr('src', 'https://www.youtube.com/embed/' + id);
-// });
+//takes a youtube video resource and adds video object to mock videos array
+function createVideoObject(data) {
+    if (data.items.length == 1) {
+        var video = {};
+        
+        video.id = data.items[0].id;
+        video.title = data.items[0].snippet.title;
+        video.channelTitle = data.items[0].snippet.channelTitle;
+        video.thumbnail = data.items[0].snippet.thumbnails.medium.url;
+        video.description = data.items[0].snippet.description;
+        video.tags = data.items[0].snippet.tags;
+        
+        MOCK_VIDEOS.videos.push(video);
+        console.log(MOCK_VIDEOS);
+    }
+}
 
+function getDataFromApi(videoId) {
+	var query = {
+		id: videoId,
+		part: 'snippet',
+		r: 'json',
+		key: 'AIzaSyCIdQfwZ7qDSA1BhnfzEBa-6AB8ma8YY9k'
+	}
+	$.getJSON(baseURL, query, function(data) {
+	    createVideoObject(data);
+	});
+}
 
-
+function onSubmit() {
+    $('#add-video-form').on('submit', function(e){
+        e.preventDefault();
+        var videoId = $(this).find('#add-video-input').val();
+        videoId = videoId.trim();
+        videoId = videoId.slice(-11);
+        getDataFromApi(videoId);
+        getAndDisplayVideos();
+    })
+}
 
 //  on page load do this
 $(function() {
 	getAndDisplayVideos();
+	onSubmit();
+	//getDataFromApi('UNr5uyJ5fKM')
 });
 
