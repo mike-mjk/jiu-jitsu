553d902d0ef568ef2d2bc64d65170d4531709aa3
Michael Kostolnik <mikeavida@gmail.com>
2017-02-24 19:36:30 +0000

    configure mongo, make video user schema

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var config = require('./config');
+
 var app = express();
 app.use(express.static('public'));
 
@@ -7,6 +12,29 @@ app.engine('html', mustacheExpress());
 app.set('view engine', 'mustache'); 
 app.set('views', __dirname + '/public');
 
+var runServer = function(callback) {
+    mongoose.connect(config.DATABASE_URL, function(err) {
+        if (err && callback) {
+            return callback(err);
+        }
+
+        app.listen(config.PORT, function() {
+            console.log('Listening on localhost:' + config.PORT);
+            if (callback) {
+                callback();
+            }
+        });
+    });
+};
+
+if (require.main === module) {
+    runServer(function(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+}
+
 app.get('/watch/:id', function(req, res){
     res.render('video.html', {
         videoID: req.params.id
@@ -14,4 +42,5 @@ app.get('/watch/:id', function(req, res){
 });
 
 exports.app = app;
+exports.runServer = runServer;
 app.listen(process.env.PORT || 8080);
