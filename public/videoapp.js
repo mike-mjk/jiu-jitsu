1c8ac7b3350db707b3799878168e6656484173c2
Michael Kostolnik <mikeavida@gmail.com>
2017-03-30 17:34:56 +0000

    user description PUT works

--- public/videoapp.js
+++ public/videoapp.js
@@ -1,21 +1,36 @@
+function getDescription() {
+    $.ajax('../videos', {
+        type: 'GET',
+        datatype: 'json'
+    })
+    .done(function(data){
+        console.log(data);
+        $('#add-description-input').val(data)
+    });
+}
+
 function submitDescription() {
     $('#add-description-form').on('submit', function(e){
         e.preventDefault();
         var description = $('#add-description-input').val();
-        
+        description = {"description": description};
         var id = $('#add-description-input').data('id');
         
         $.ajax('/videos/'+ id + '/description', {
         type: 'PUT',
-        data: {description: description}
+        data: JSON.stringify(thing),
+        dataType: 'json',
+        contentType: 'application/json'
     })
+    
     .done(function(){
-        
+        getDescription();
     });
     
     });
 }
 
 $(function() {
+    getDescription();
     submitDescription();
 })
