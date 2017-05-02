9e9c1a40c5ba238f8338c0e940276ccfae9bdad1
Michael Kostolnik <mikeavida@gmail.com>
2017-05-02 20:14:00 +0000

    test

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/jiu-jitsu' :
+                            'mongodb://mikeavida:password@ds115071.mlab.com:15071/heroku_m7klh9np' :
                             'mongodb://localhost/jiu-jitsu-dev');
-                            exports.DATABASE_URL = 'mongodb://mikeavida:password@ds115071.mlab.com:15071/heroku_m7klh9np';
+// exports.DATABASE_URL = 'mongodb://mikeavida:password@ds115071.mlab.com:15071/heroku_m7klh9np';
 exports.PORT = process.env.PORT || 8080;
